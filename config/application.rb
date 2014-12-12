f2b09a756adcb3af0967e0f9c88fa3f229690cb8
Vasilly <vasko.traikovski@gmail.com>
2014-12-13 01:43:46 +1100

    first commit

---
+++ config/application.rb
@@ -0,0 +1,16 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Receta
+  class Application < Rails::Application
+   config.assets.paths << Rails.root.join("vendor","assets","bower_components")
+   config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts")
+
+   config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff)$)
+  end
+end
